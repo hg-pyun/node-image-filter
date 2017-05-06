a6ea705ead6b743d015529f99ca5e56d16913ddf
hg.pyun <hg.pyun@linecorp.com>
2017-05-06 14:46:48 +0900

    update prototype

--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
-// default moudle
+// default module
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-// lib
-const Filter = require('../src');
+// filter lib
+const Filter = require('../lib');
 
 const app = express();
 
-app.use(function (request, response, next) {
-    Filter.getPixels(path.join(__dirname, '../test/image/cat.jpg'), function (err, pixels) {
-        if (err) {
-            console.log('err', err);
-            return;
+app.use(function (req, res, next) {
+
+    let imagePath = path.join(__dirname, '../test/cat.jpg');
+    let invertFilter = function (pixels) {
+        let d = pixels;
+        for(let i=0; i<pixels.length; i+=4 ){
+            d[i] = 255 - d[i];
+            d[i+1] = 255 - d[i+1];
+            d[i+2] = 255 - d[i+2];
+            d[i+3] = 255;
         }
+        return pixels;
+    };
 
-        console.log('pixels', pixels);
-        console.log(pixels.data[0], pixels.data[1], pixels.data[2], pixels.data[3]);
-        Filter.preset.invert(pixels);
-        console.log(pixels.data[0], pixels.data[1], pixels.data[2], pixels.data[3]);
-
-    });
+    Filter.render(imagePath, invertFilter, function (result) {
+        fs.writeFile(`result.${result.type}`, result.data);
+        res.send('save filtered image');
+    })
 });
+
 
 app.listen(3000, function () {
     console.log('Server Running at http://127.0.0.1:3000');
