5ae8cc41da8c671318a28fa5a2f82883f387f459
haegul <phg2491@gmail.com>
2017-08-06 17:52:18 +0900

    fixed bug #6 https://github.com/haegul/node-image-filter/issues/6

--- tools/server/devServer.js
+++ tools/server/devServer.js
@@ -8,36 +8,38 @@ const Filter = require('../../lib');
 
 const app = express();
 
-app.get('/favicon.ico', function(req, res) {
+app.get('/favicon.ico', function (req, res) {
     res.status(204);
 });
 
 app.use('/', function (req, res, next) {
 
-    let imagePathJPG = path.join(__dirname, '../samples/cat.jpg');
+    const imagePathJPG = path.join(__dirname, '../samples/cat.jpg');
+    const imagePathPNG = path.join(__dirname, '../samples/cat.png');
 
     Filter.render(imagePathJPG, Filter.preset.invert, function (result) {
-        result.data.pipe(fs.createWriteStream(`result.${result.type}`));
-        console.log('[DEV Server]', 'Saved JPG');
+        result.data.pipe(fs.createWriteStream(`result.invert.${result.type}`));
+        console.log('[DEV Server]', 'Saved Invert JPG');
     });
-
-    let imagePathPNG = path.join(__dirname, '../samples/cat.png');
 
     Filter.render(imagePathPNG, Filter.preset.invert, function (result) {
-        result.data.pipe(fs.createWriteStream(`result.${result.type}`));
-        console.log('[DEV Server]', 'Saved PNG');
+        result.data.pipe(fs.createWriteStream(`result.invert.${result.type}`));
+        console.log('[DEV Server]', 'Saved Invert PNG');
     });
 
-    // convolutions
-    function sobel(pixels) {
-        return Filter.convolution(pixels,
-              [ -1,  0, 1,
-                -2,  0, 2,
-                -1,  0, 1], 1);
-    }
-    Filter.render(imagePathJPG, sobel, function (result) {
+    Filter.render(imagePathJPG, Filter.preset.sobel, function (result) {
         result.data.pipe(fs.createWriteStream(`result.sobel.${result.type}`));
-        console.log('[DEV Server]', 'Saved Convolution JPG');
+        console.log('[DEV Server]', 'Saved Sobel JPG');
+    });
+
+    Filter.render(imagePathJPG, Filter.preset.sharpen, function (result) {
+        result.data.pipe(fs.createWriteStream(`result.sharpen.${result.type}`));
+        console.log('[DEV Server]', 'Saved Sobel JPG');
+    });
+
+    Filter.render(imagePathJPG, Filter.preset.blur, function (result) {
+        result.data.pipe(fs.createWriteStream(`result.blur.${result.type}`));
+        console.log('[DEV Server]', 'Saved Sobel JPG');
     });
 
     console.log('[DEV Server]', 'Send Response');
